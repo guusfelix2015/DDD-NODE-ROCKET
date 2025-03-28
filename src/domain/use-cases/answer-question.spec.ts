b6a1ab25be52d90d6bfa21a1c1f88cb2ee12564f
guusfelix2015 <gusfelix2015@gmail.com>
2025-03-28 14:57:33 -0300

    Dependências externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,19 @@
 import {expect, test} from "vitest";
 import {AnswerQuestionUseCase} from "./answer-question";
+import {AnswersRepository} from "../repositories/answers-repository";
+import {Answer} from "../entities/answer";
+
+const fakeAnswerRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  }
+}
 
 
 test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-  const answer = answerQuestion.execute({
+  const answer = await  answerQuestion.execute({
     questionId: "123",
     instructorId: "123",
     content: "Nova resposta"
