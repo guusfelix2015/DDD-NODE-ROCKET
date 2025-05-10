76bf9c78a5f550c9d5843b7a90a108687ecd8926
guusfelix2015 <gusfelix2015@gmail.com>
2025-05-09 21:45:05 -0300

    Abstraindo criação de entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
-import {Entity} from "../../core/entities/entity";
+import { Entity } from "../../core/entities/entity";
 import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 
 interface AnswerProps {
     authorId: UniqueEntityID,
@@ -9,8 +10,17 @@ interface AnswerProps {
     updatedAt?: Date,
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends Entity<AnswerProps> {   
     get content() {
         return this.props.content;
+    }
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date(),
+        }, id)
+
+        return answer
     }
 }
--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,8 +1,13 @@
 import {Entity} from "../../core/entities/entity";
-
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 interface InstructorProps {
     name: string
 }
 
-class Instructor extends Entity<InstructorProps> {
+export class Instructor extends Entity<InstructorProps> {
+    static create(props: InstructorProps, id?: UniqueEntityID) {
+        const instructor = new Instructor(props, id)
+
+        return instructor
+    }
 }
